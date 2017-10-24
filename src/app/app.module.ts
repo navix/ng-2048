d2487668161aff19fcb9ebd3249f82b994fba643
navix <inavix@gmail.com>
2017-10-24 11:34:08 +0300

    feat: update to angular@4.4.6 and angular-cli@1.4.9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FieldModule } from './field/field.module';
+import { FieldComponent } from './field/field/field.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    BrowserAnimationsModule,
     FieldModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: FieldComponent,
+      },
+    ]),
   ],
   declarations: [
     AppComponent,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {
 }
